31826d13677268a8123f5592e93417ca7497768f
Armando Perez <gmandx@gmail.com>
2017-08-22 12:44:54 -0700

    More bundling tweaks, improvements for CSS and image handling

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,61 @@
-import rollupBabel from 'rollup-plugin-babel';
-import rollupResolve from 'rollup-plugin-node-resolve';
-import rollupEslint from 'rollup-plugin-eslint';
-import rollupCommonjs from 'rollup-plugin-commonjs';
-import rollupUglify from 'rollup-plugin-uglify';
+import path from 'path';
+
+// Rollup-related imports
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import eslint from 'rollup-plugin-eslint';
+import commonjs from 'rollup-plugin-commonjs';
+import uglify from 'rollup-plugin-uglify';
+import replace from 'rollup-plugin-replace';
+import postcss from 'rollup-plugin-postcss';
+
+// PostCSS-related imports
+import cssnano from 'cssnano';
+import cssUrl from 'postcss-url';
+import cssUrlCopy from 'postcss-url/src/type/copy';
+import cssAutoprefixer from 'autoprefixer';
+import cssPreCss from 'precss';
 
 // `npm run build` -> `production` is true
 // `npm run dev` -> `production` is false
 const production = !process.env.ROLLUP_WATCH;
+const NODE_ENV = process.env.NODE_ENV || (
+  process.env.ROLLUP_WATCH ? 'development' : 'production');
+
+
+function noop(){}
+
 
 export default {
   entry: 'src/main.js',
   dest: 'public/bundle.js',
   format: 'iife', // immediately-invoked function expression — suitable for <script> tags
   plugins: [
-    rollupEslint({ exclude: 'node_modules/**' }),
-    rollupBabel({ exclude: 'node_modules/**', }),
-    rollupResolve({ jsnext: true }), // tells Rollup how to find date-fns in node_modules
-    rollupCommonjs(), // converts date-fns to ES modules
-    production && rollupUglify(), // minify, but only in production
+    postcss({
+      plugins: [
+        cssPreCss(),
+        cssAutoprefixer(),
+        production ? cssnano() : noop,
+        cssUrl({
+          basePath: path.resolve(__dirname, 'src/'),  // base path to search assets from
+          assetsPath: path.resolve(__dirname, 'public/'),  // dir to copy assets
+          useHash: true,  // using hash names for assets (generates from asset content)
+          url() {
+            const url = cssUrlCopy(...arguments);
+            return url && path.basename(url);
+          },
+        }),
+      ],
+      extract: true,
+    }),
+    eslint({ exclude: 'node_modules/**' }),
+    babel({ exclude: 'node_modules/**', }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
+    }),
+    resolve({ jsnext: true }),  // tells Rollup how to find date-fns in node_modules
+    commonjs(),  // converts CJS to ES modules
+    production && uglify(),  // minify, but only in production
   ],
   sourceMap: true,
 };
